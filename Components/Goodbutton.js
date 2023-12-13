41043270c29e6977df333a6bbd9e3c05eb320cf1
1112luke <128310124+1112luke@users.noreply.github.com>
2023-12-12 23:58:55 -0500

    organization

---
+++ Components/Goodbutton.js
@@ -0,0 +1,25 @@
+import { Pressable, Text } from "react-native";
+import { StyleSheet } from "react-native";
+
+export function Goodbutton(props) {
+  return (
+    <>
+      <Pressable onPress={props.onpress} style={styles.button}>
+        <Text style={{ padding: 10, textAlign: "center", color: "lightgray" }}>
+          Sign In
+        </Text>
+      </Pressable>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    height: 40,
+    backgroundColor: "grey",
+    width: 300,
+    borderRadius: 5,
+    textAlign: "center",
+    marginTop: 15,
+  },
+});

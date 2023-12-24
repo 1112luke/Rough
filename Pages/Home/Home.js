c0414c6b7e511c2d9f796890b8acc17b18bdc802
BuildTools <unconfigured@null.spigotmc.org>
2023-12-24 11:23:16 -0500

    updates

--- Pages/Home/Home.js
+++ Pages/Home/Home.js
@@ -1,4 +1,11 @@
-import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    ScrollView,
+    FlatList,
+} from "react-native";
 import { useEffect, useState } from "react";
 import { Topbar } from "../../Components/Topbar";
 import { Listingbox } from "./Listingbox";
@@ -8,8 +15,13 @@ import global from "../../style";
 
 export function Home({ navigation }) {
     const [listings, setlistings] = useState([]);
+    const [refreshing, setrefreshing] = useState(false);
+
+    var refresh = 1;
 
     //get listings -- eventually this will be a more advanced query
+    //todo eventually remove onsnapshot and make a get rather than listener. bind this to pullup refresh.
+    //todo make sure all items are visible at bottom of page // potentially insert blank elements into listings before render
     useEffect(() => {
         const listingref = collection(db, "listings");
 
@@ -28,6 +40,18 @@ export function Home({ navigation }) {
         });
     }, []);
 
+    //all this is only to test pullup on refresh
+    function timeout(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    async function Refresh() {
+        refresh++;
+        setrefreshing(true);
+        await timeout(3000);
+        setrefreshing(false);
+    }
+
     function goToPage(listing, img) {
         navigation.navigate("Listingpage", { listing: listing, image: img });
     }
@@ -36,46 +60,47 @@ export function Home({ navigation }) {
         <>
             <SafeAreaView style={[global.creme, { flex: 1 }]}>
                 <Topbar navigation={navigation}></Topbar>
-                <ScrollView>
+                {listings.length == 0 ? (
                     <View style={styles.container}>
-                        {listings == [] ? (
-                            <>
-                                <View
-                                    style={{
-                                        flex: 1,
-                                        justifyContent: "center",
-                                        alignItems: "center",
-                                    }}
-                                >
-                                    <Text
-                                        style={[
-                                            global.font,
-                                            {
-                                                fontSize: 30,
-                                                textAlign: "center",
-                                            },
-                                        ]}
-                                    >
-                                        Nothing Here...
-                                    </Text>
-                                </View>
-                            </>
-                        ) : (
-                            listings.map((listing, i) => {
-                                return (
-                                    <Listingbox
-                                        listing={listing}
-                                        goToPage={goToPage}
-                                        key={i}
-                                    ></Listingbox>
-                                );
-                            })
-                        )}
-                        <View style={{ height: 300 }}>
-                            <Text style={{ color: "white" }}>.</Text>
+                        <View
+                            style={{
+                                flex: 1,
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Text
+                                style={[
+                                    global.font,
+                                    {
+                                        fontSize: 30,
+                                        textAlign: "center",
+                                    },
+                                ]}
+                            >
+                                Nothing Here...
+                            </Text>
                         </View>
                     </View>
-                </ScrollView>
+                ) : (
+                    <FlatList
+                        data={listings}
+                        numColumns={2}
+                        onRefresh={Refresh}
+                        initialNumToRender={1}
+                        refreshing={refreshing}
+                        renderItem={({ item, index }) => {
+                            return (
+                                <Listingbox
+                                    listing={item}
+                                    goToPage={goToPage}
+                                    mode="info"
+                                    key={index}
+                                ></Listingbox>
+                            );
+                        }}
+                    />
+                )}
             </SafeAreaView>
         </>
     );
@@ -85,5 +110,6 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
         flexWrap: "wrap",
+        flex: 1,
     },
 });
